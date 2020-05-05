ea892873b55b6999dae233a36a9c482b9df6b3e3
Le Ba Vui <lebavui@gmail.com>
2020-05-05 20:23:43 +0700

    Add chat server and client

---
+++ samples/ChatClient/ChatClient/resource.h
@@ -0,0 +1,40 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by ChatClient.rc
+//
+#define IDC_MYICON                      2
+#define IDD_CHATCLIENT_DIALOG           102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_CHATCLIENT                  107
+#define IDI_SMALL                       108
+#define IDC_CHATCLIENT                  109
+#define IDC_LIST_MESSAGE                110
+#define IDC_LIST_CLIENT                 111
+#define IDC_EDIT_MESSAGE                112
+#define IDC_EDIT_MESSAGE_PRIVATE        112
+#define IDC_BUTTON_SEND                 113
+#define IDR_MAINFRAME                   128
+#define IDD_DIALOG_LOGIN                129
+#define IDD_DIALOG_CONNECT              129
+#define IDD_DIALOG1                     130
+#define IDD_DIALOG_CHAT                 131
+#define IDC_EDIT_CLIENT_ID              1000
+#define IDC_STATIC_RESULT               1001
+#define ID_FILE_CONNECT                 32773
+#define ID_FILE_DISCONNECT              32774
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        132
+#define _APS_NEXT_COMMAND_VALUE         32775
+#define _APS_NEXT_CONTROL_VALUE         1008
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
